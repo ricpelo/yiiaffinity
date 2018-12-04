9fff80fde98e078ff4d7f1327c6436c9788ad345
Ricardo Pérez <ricpelo@gmail.com>
2018-12-04 20:57:44 +0100

    Valida los géneros únicos

--- models/GenerosForm.php
+++ models/GenerosForm.php
@@ -2,6 +2,7 @@
 
 namespace app\models;
 
+use Yii;
 use yii\base\Model;
 
 class GenerosForm extends Model
@@ -13,6 +14,16 @@ class GenerosForm extends Model
         return [
             [['genero'], 'required'],
             [['genero'], 'string', 'max' => 255],
+            [['genero'], function ($attribute, $params, $validator) {
+                $fila = Yii::$app->db
+                    ->createCommand('SELECT id
+                                       FROM generos
+                                      WHERE genero = :genero', [':genero' => $this->$attribute])
+                    ->queryOne();
+                if (!empty($fila) && $fila['id'] != Yii::$app->request->get('id')) {
+                    $this->addError($attribute, 'Ese género ya existe.');
+                }
+            }],
         ];
     }
 }
