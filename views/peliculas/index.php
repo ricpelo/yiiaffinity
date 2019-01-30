22246cb7a6acb9ea7b1cfce1b9e926d084328d2d
Ricardo Pérez <ricpelo@gmail.com>
2019-01-30 20:52:13 +0100

    Extensión yii2-number y código JS

--- views/peliculas/index.php
+++ views/peliculas/index.php
@@ -3,11 +3,34 @@ use yii\grid\GridView;
 use yii\grid\DataColumn;
 use yii\grid\ActionColumn;
 use yii\grid\SerialColumn;
+use kartik\number\NumberControl;
+use yii\helpers\Html;
+
+use yii\web\View;
 
 $this->title = 'Listado de películas';
 $this->params['breadcrumbs'][] = $this->title;
+$js = <<<EOF
+    $('#boton').click(function (ev) {
+        ev.preventDefault();
+        alert('hola');
+    });
+EOF;
+$this->registerJs($js);
 ?>
 <h1>Listado de películas</h1>
+
+<button id="boton" type="button" name="button">Púlsame</button>
+
+<?= NumberControl::widget([
+    'name' => 'prueba',
+    'value' => 1200,
+    'maskedInputOptions' => [
+        'suffix' => ' €',
+        'groupSeparator' => '.',
+        'radixPoint' => ','
+    ],
+]) ?>
 
 <?= GridView::widget([
     'dataProvider' => $dataProvider,
@@ -23,7 +46,16 @@ $this->params['breadcrumbs'][] = $this->title;
             },
             'format' => 'duration',
         ],
-        'genero.genero',
+        [
+            'attribute' => 'genero.genero',
+            'value' => function ($model) {
+                return Html::a(
+                    Html::encode($model->genero->genero),
+                    ['generos/ver', 'id' => $model->genero->id]
+                );
+            },
+            'format' => 'raw',
+        ],
         [
             'class' => ActionColumn::class,
             'header' => 'Acciones',
